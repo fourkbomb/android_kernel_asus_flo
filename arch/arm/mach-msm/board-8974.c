b901143656743ea44a4614227eca3d4a6194db7f
Neeti Desai <neetid@codeaurora.org>
2012-11-01 21:57:36 -0700

    msm: 8974: Enable DONT_MAP_HOLE_AFTER_MEMBANK0
    If this config option is enabled, the kernel does not map the
    memory corresponding to the largest hole into the virtual
    memory resulting in more lowmem. If multiple holes exist, the
    largest hole in the first 256MB of the memory is not mapped.
    
    In device tree based targets the kernel is no longer dependant
    on the bootloader for the start and size of the memory holes.
    Thus the meminfo is adjusted to reflect the memory hole, if it
    is present, using the start and end of the memory hole, while
    parsing the device tree.
    
    For non-device tree based targets, the meminfo is used to find
    the start and size of the memory hole. No adjustment is needed
    to be made to the meminfo, as the hole is taken into account,
    based on the memory information passed by the atags.
    
    Change-Id: Ib2619f72ac2b4142330534c8bbe1c7d9f64ea38c
    Signed-off-by: Neeti Desai <neetid@codeaurora.org>
    [zwliew: Don't really mess with arch/arm/mach-msm/board-8974.c]
    Signed-off-by: Zhao Wei Liew <zhaoweiliew@gmail.com>

--- arch/arm/mach-msm/board-8974.c
+++ arch/arm/mach-msm/board-8974.c
@@ -257,6 +257,13 @@ static struct reserve_info msm_8974_reserve_info __initdata = {
 	.paddr_to_memtype = msm_8974_paddr_to_memtype,
 };
 
+void __init msm_8974_reserve(void)
+{
+	reserve_info = &msm_8974_reserve_info;
+	of_scan_flat_dt(dt_scan_for_memory_reserve, msm_8974_reserve_table);
+	msm_reserve();
+}
+
 static void __init msm_8974_early_memory(void)
 {
 	reserve_info = &msm_8974_reserve_info;
