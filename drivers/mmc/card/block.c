975d16a19eab6961f7d1d86b1462c69eb57b98cf
hsuan-chih_chen <hsuan-chih_chen@asus.com>
2012-12-04 11:16:11 +0800

    mmc: add SECURE DISCARD CONFIG
    default disable
    
    Change-Id: Iabc7c21c87f233a1ca3e2bb0a31ad03749a1cf08
    Reviewed-on: http://mcrd1-5.corpnet.asus/code-review/master/63330
    Reviewed-by: Hsuan-Chih Chen <hsuan-chih_chen@asus.com>
    Tested-by: Hsuan-Chih Chen <hsuan-chih_chen@asus.com>
    Reviewed-by: Yeh Paris <paris_yeh@asus.com>
    Tested-by: Yeh Paris <paris_yeh@asus.com>
    Reviewed-by: Sam hblee <Sam_hblee@asus.com>
    Reviewed-on: http://mcrd1-5.corpnet.asus/code-review/master/67851
    Reviewed-by: Lin Johnny1 <Johnny1_Lin@asus.com>
    Reviewed-by: Nick Cheng <nick_cheng@asus.com>
    Tested-by: Nick Cheng <nick_cheng@asus.com>

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -983,6 +983,7 @@ out:
 	return err ? 0 : 1;
 }
 
+#ifdef CONFIG_MMC_SECDISCARD
 static int mmc_blk_issue_secdiscard_rq(struct mmc_queue *mq,
 				       struct request *req)
 {
@@ -1048,7 +1049,7 @@ out:
 
 	return err ? 0 : 1;
 }
-
+#endif
 static int mmc_blk_issue_sanitize_rq(struct mmc_queue *mq,
 				      struct request *req)
 {
@@ -2129,10 +2130,13 @@ static int mmc_blk_issue_rq(struct mmc_queue *mq, struct request *req)
 		/* complete ongoing async transfer before issuing discard */
 		if (card->host->areq)
 			mmc_blk_issue_rw_rq(mq, NULL);
+
+#ifdef CONFIG_MMC_SECDISCARD
 		if (req->cmd_flags & REQ_SECURE &&
 			!(card->quirks & MMC_QUIRK_SEC_ERASE_TRIM_BROKEN))
 			ret = mmc_blk_issue_secdiscard_rq(mq, req);
 		else
+#endif
 			ret = mmc_blk_issue_discard_rq(mq, req);
 	} else if (req && req->cmd_flags & REQ_FLUSH) {
 		/* complete ongoing async transfer before issuing flush */
