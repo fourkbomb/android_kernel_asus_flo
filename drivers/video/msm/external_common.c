a3b3689f52d7c994bea8c5721cf5034ddb5b866c
Ajay Singh Parmar <aparmar@codeaurora.org>
2013-03-19 13:00:57 +0530

    msm_fb: hdmi: Remove supported resolution check
    The change will remove the supported resolution check while turning
    on the HDMI. The supported resolution is updated on parsing EDID for
    VIC and matching it with default mode in HDMI. This may fail in case
    the default video mode is not available in EDID of TV resulting in
    HDMI turn on failure.
    
    Also, this check is not needed because we now have the HDMI display
    pipe line check which will make sure we do not turn on HDMI if it
    is not supposed to be turned ON.
    
    CRs-Fixed: 461486
    Change-Id: I09ba863cb6f56c6c5d599b91516855f0540f17c3
    Signed-off-by: Ajay Singh Parmar <aparmar@codeaurora.org>

--- drivers/video/msm/external_common.c
+++ drivers/video/msm/external_common.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010-2012, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2010-2013, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1402,7 +1402,6 @@ static void add_supported_video_format(
 			DEV_DBG("%s: Default resolution %d [%s] supported\n",
 					__func__, video_format,
 					msm_hdmi_mode_2string(video_format));
-			external_common_state->default_res_supported = true;
 		}
 	}
 }
@@ -1906,7 +1905,6 @@ int hdmi_common_read_edid(void)
 		sizeof(external_common_state->spkr_alloc_data_block));
 	external_common_state->adb_size = 0;
 	external_common_state->sadb_size = 0;
-	external_common_state->default_res_supported = false;
 
 	status = hdmi_common_read_edid_block(0, edid_buf);
 	if (status || !check_edid_header(edid_buf)) {
@@ -2012,7 +2010,6 @@ error:
 	external_common_state->disp_mode_list.num_of_elements = 1;
 	external_common_state->disp_mode_list.disp_mode_list[0] =
 		external_common_state->video_resolution;
-	external_common_state->default_res_supported = true;
 	return status;
 }
 EXPORT_SYMBOL(hdmi_common_read_edid);
