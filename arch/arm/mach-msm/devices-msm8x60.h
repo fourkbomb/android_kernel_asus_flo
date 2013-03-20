c9ffd671952ec497cff1b8a669afdcdb176cf0f1
Mekala Natarajan <mekalan@codeaurora.org>
2013-03-19 18:04:29 -0700

    ASoC: msm: Load ADSP image during bootup
    On 8064, ADSP image is loaded when pcm_open is
    called for the first time. This caused the first
    audio playback to be delayed. To fix this, adsp_loader
    is modified to load ADSP firmware using pil.
    
    Bug: 8204207
    
    Change-Id: I302a9c27adc252df4a13106ee75bed448e400455
    Signed-off-by: Alexy Joseph <alexyj@codeaurora.org>
    Signed-off-by: Mekala Natarajan <mekalan@codeaurora.org>
    Signed-off-by: Vineeta Srivastava <vsrivastava@google.com>
    Signed-off-by: Ed Tam <etam@google.com>

--- arch/arm/mach-msm/devices-msm8x60.h
+++ arch/arm/mach-msm/devices-msm8x60.h
@@ -58,6 +58,7 @@ extern struct platform_device msm_device_gpio;
 extern struct platform_device msm_device_vidc;
 extern struct platform_device apq8064_msm_device_vidc;
 extern struct platform_device apq8064_msm_ba_device;
+extern struct platform_device adsp_loader_device;
 
 extern struct platform_device msm_charm_modem;
 extern struct platform_device msm_device_tz_log;
