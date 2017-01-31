MotorolaMobilityLLC/kernel-msm

deb: initial bring up the deb hardware
Change-Id: I428beb5964726f009cd7a6402de0e83f0cf9924f
Signed-off-by: paris_yeh <paris_yeh@asus.com>
Reviewed-on: http://mcrd1-5.corpnet.asus/code-review/master/67828
Reviewed-by: Sam hblee <Sam_hblee@asus.com>

sound/soc/msm/apq8064.c
@@ -1225,7 +1225,7 @@ static int msm_audrx_init(struct snd_soc_pcm_runtime *rtd)
 		ARRAY_SIZE(apq8064_common_audio_map));
 
 	if (machine_is_apq8064_mtp() || machine_is_apq8064_mako()
-		|| machine_is_apq8064_flo()) {
+		|| machine_is_apq8064_flo() || machine_is_apq8064_deb()) {
 		snd_soc_dapm_add_routes(dapm, apq8064_mtp_audio_map,
 			ARRAY_SIZE(apq8064_mtp_audio_map));
 	} else  {
@@ -2142,7 +2142,8 @@ static int __init msm_audio_init(void)
 	u32	version = socinfo_get_platform_version();
 	if (!soc_class_is_apq8064() ||
 		(socinfo_get_id() == 130) ||
-		((machine_is_apq8064_mtp() || machine_is_apq8064_flo()) &&
+		((machine_is_apq8064_mtp() || machine_is_apq8064_flo() ||
+		machine_is_apq8064_deb()) &&
 		(SOCINFO_VERSION_MINOR(version) == 1))) {
 		pr_info("%s: Not APQ8064 in SLIMBUS mode\n", __func__);
 		return -ENODEV;

fourkbomb/android_kernel_asus_flo

sound/soc/msm/apq8064.c
@@ -1261,7 +1261,7 @@ static int msm_audrx_init(struct snd_soc_pcm_runtime *rtd)
 		ARRAY_SIZE(apq8064_common_audio_map));
 
 	if (machine_is_apq8064_mtp()
-		|| machine_is_apq8064_flo()) {
+		|| machine_is_apq8064_flo() || machine_is_apq8064_dev()) {
 		snd_soc_dapm_add_routes(dapm, apq8064_mtp_audio_map,
 			ARRAY_SIZE(apq8064_mtp_audio_map));
 	} else  {
@@ -2249,7 +2249,8 @@ static int __init msm_audio_init(void)
 	u32	version = socinfo_get_platform_version();
 	if (!soc_class_is_apq8064() ||
 		(socinfo_get_id() == 130) ||
-		((machine_is_apq8064_mtp() || machine_is_apq8064_flo()) &&
+		((machine_is_apq8064_mtp() || machine_is_apq8064_flo() ||
+		machine_is_apq8064_deb()) &&
 		(SOCINFO_VERSION_MINOR(version) == 1)) ||
 		machine_is_apq8064_adp_2() || machine_is_apq8064_adp2_es2() ||
 		machine_is_apq8064_adp2_es2p5()) {